930c66662e5863925b0f38e780d7276080542a9e
Titidafi <draggon1998@gmail.com>
2017-11-26 19:12:22 +0100

    Remember me graphical update

--- vues/login.php
+++ vues/login.php
@@ -10,8 +10,10 @@
         <div class="form-group">
         <label for="mdp">Votre mot de passe : </label><input type="password" id="mdp" name="mdp" class="form-control">
         </div>
-        <div class="form-group">
-            <label for="rm">Remember me <input class="checkbox-inline" type="checkbox" name="rm" id="rm"></label>
+        <div class="checkbox">
+             <label for="rm">
+                 <input class="checkbox" type="checkbox" name="rm" id="rm">Se rappeler de moi
+             </label>
         </div>
         <input type="submit" class="btn btn-success btn-block" value="Connexion"  >
 
