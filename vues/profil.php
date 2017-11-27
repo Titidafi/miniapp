48e175a355253bbe2fd2e3c6fbcfe3a4e464a58f
Titidafi <draggon1998@gmail.com>
2017-11-27 11:51:58 +0100

    Correction

--- vues/profil.php
+++ vues/profil.php
@@ -5,10 +5,9 @@
 
             <?php
 
-            $sql = "SELECT DISTINCT * FROM ecrit JOIN user ON user.id=ecrit.idAmi";
+            $sql = "SELECT DISTINCT login FROM user WHERE  id=?";
             $querry = $pdo->prepare($sql);
-            $querry->execute();
-            $nombre = [];
+            $querry->execute(array($_GET['id']));
 
             ?>
 
@@ -17,10 +16,6 @@
 </div>
 <div class="col-md-4">
 <?php
-while ($line = $querry->fetch()) {
-    array_push($nombre, $line['id']);
-
-}
 if (in_array($_GET['id'], $nombre)) {
     $sql = "SELECT DISTINCT login, user.id, etat, idUtilisateur1, idUtilisateur2 FROM user JOIN ecrit ON idAuteur=user.id JOIN lien ON ecrit.idAuteur = lien.idUtilisateur2 WHERE idUtilisateur1 =? OR idUtilisateur2=?";
     $querry = $pdo->prepare($sql);
@@ -47,21 +42,22 @@ if (in_array($_GET['id'], $nombre)) {
 }
 if ($loginUser != $_SESSION["login"]) {
 
-
+echo $loginUser;
+echo $_SESSION['login']
     ?>
     <div class="card thumbnail" style="width:'20rem';">
         <img class="card-img-top" src="12.png"  alt="img"/>
         <div class="card-body">
-            <h4 class="card-title"><?php echo $loginUser;?></h4>
+            <h4 class="card-title">
+            </h4>
             <p class="card-text">Avec du texte pour dire voilà</p>
             <?php affichage_bouton_profil($pdo,$_SESSION['id'],$_GET['id']);
             ?>
         </div>
     </div>
 
-    <?php }else{
-    header("Location:index.php?action=compte");
-}
+    <?php }
 ?>
 
+    <h2>YES LIFE</h2 >
 </div >
